ccca70b962b38b65d8c5a9014dda5803356f0bcf
Silas Wieberneit <swieberneit@bytesunlimited.net>
2026-09-22 11:26:23 +0200

    test: Improve tests to verify output

--- tests/test_yara_generator.py
+++ tests/test_yara_generator.py
@@ -1,4 +1,5 @@
 # test_yara_generator.py
+import logging
 
 import pytest
 from unittest.mock import MagicMock
@@ -7,47 +8,72 @@ from pymisp import PyMISP
 import yara_generator
 
 @pytest.fixture
-def misp(monkeypatch) -> PyMISP:
+def misp(monkeypatch) -> MagicMock:
     """Mock the MISP API so no requests are made."""
     mock = MagicMock(spec=PyMISP)
     monkeypatch.setattr(yara_generator, "PyMISP", MagicMock(return_value=mock))
     return mock
 
-def test_no_events(misp):
+def test_no_events(misp, caplog):
     misp.search.return_value = []
 
-    yara_generator.main()
+    with caplog.at_level(logging.INFO):
+        yara_generator.main()
     misp.search.assert_called_once()
 
-def test_no_attributes(misp, misp_event):
+    assert "Events processed: 0\n" in caplog.text
+    assert "YARA attributes created: 0\n" in caplog.text
+
+def test_no_attributes(misp, caplog, misp_event):
     misp.search.return_value = [misp_event]
+    misp.get_event.return_value = misp_event
 
-    yara_generator.main()
+    with caplog.at_level(logging.INFO):
+        yara_generator.main()
     misp.search.assert_called_once()
 
-def test_one_attribute(misp, misp_event, misp_attribute):
+    assert "Events processed: 1\n" in caplog.text
+    assert "YARA attributes created: 0\n" in caplog.text
+
+def test_one_attribute(misp, caplog, misp_event, misp_attribute):
     misp_event.attributes = [misp_attribute]
     misp.search.return_value = [misp_event]
+    misp.get_event.return_value = misp_event
 
-    yara_generator.main()
+    with caplog.at_level(logging.INFO):
+        yara_generator.main()
     misp.search.assert_called_once()
 
-def test_hundred_attribute(misp, misp_event, misp_attribute):
+    assert "Events processed: 1\n" in caplog.text
+    assert "YARA attributes created: 1\n" in caplog.text
+
+def test_hundred_attribute(misp, caplog, misp_event, misp_attribute):
     misp_event.attributes = []
     for _ in range(100):
         misp_event.attributes.append(misp_attribute)
     misp.search.return_value = [misp_event]
+    misp.get_event.return_value = misp_event
 
-    yara_generator.main()
+    with caplog.at_level(logging.INFO):
+        assert len(misp_event.attributes) == 100
+        yara_generator.main()
     misp.search.assert_called_once()
 
+    assert "Events processed: 1\n" in caplog.text
+    assert "YARA attributes created: 100\n" in caplog.text
 
-def test_hundred_events_with_each_one_attribute(misp, misp_event, misp_attribute):
+
+def test_hundred_events_with_each_one_attribute(misp, caplog, misp_event, misp_attribute):
     misp_event.attributes = [misp_attribute]
     misp_events = []
     for _ in range(100):
         misp_events.append(misp_event)
     misp.search.return_value = misp_events
+    misp.get_event.return_value = misp_event
 
-    yara_generator.main()
+    with caplog.at_level(logging.INFO):
+        yara_generator.main()
     misp.search.assert_called_once()
+
+    assert "Events processed: 100\n" in caplog.text
+    assert "YARA attributes created: 100\n" in caplog.text
--- tests/test_yara_scan.py
+++ tests/test_yara_scan.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 import yara_scan
 
-def test_no_match(monkeypatch, tmp_path):
+def test_no_match(monkeypatch, tmp_path, capsys):
     monkeypatch.setattr(yara_scan, "yara_dir", tmp_path / "yara_dir")
 
     yara_file = Path(f"{yara_scan.yara_dir}/test.yar")
@@ -13,3 +13,21 @@ def test_no_match(monkeypatch, tmp_path):
     test_file = tmp_path / "test.exe"
     test_file.write_text("test")
     yara_scan.main(test_file)
+
+    captured = capsys.readouterr()
+    assert "Event ID" not in captured.out
+
+
+def test_match(monkeypatch, tmp_path, capsys):
+    monkeypatch.setattr(yara_scan, "yara_dir", tmp_path / "yara_dir")
+
+    yara_file = Path(f"{yara_scan.yara_dir}/test.yar")
+    yara_file.parent.mkdir(parents=True, exist_ok=True)
+    yara_file.write_text('rule Dummy { meta: event_id = 1 attr_uuid = "" condition: true }')
+
+    test_file = tmp_path / "test.exe"
+    test_file.write_text("test")
+    yara_scan.main(test_file)
+
+    captured = capsys.readouterr()
+    assert "Event ID" in captured.out
